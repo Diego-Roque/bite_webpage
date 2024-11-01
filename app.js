2b3ab2fb5b801ec7d3d40ac0528748edd97fab26
diego <diego.roque0309@gmail.com>
2024-10-31 21:42:40 -0600

    animation

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/animation.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/hiw', (req, res) => {
